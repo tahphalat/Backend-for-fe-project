e97f65ff270779855a1da8197897bf9cc27706e1
mokunato <mokunato@gmail.com>
2025-03-25 13:11:27 +0700

    bug fix

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 //Route files
-const hospitals = require('./routes/hospitals')
+const cars = require('./routes/cars')
 const auth = require('./routes/auth');
-const appointments = require('./routes/appointments');
+const bookings = require('./routes/bookings');
 const app = express();
 const mongoSanitize = require('express-mongo-sanitize');
 
@@ -42,7 +42,7 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(hpp());
 app.use(cors());
-
+const PORT = process.env.PORT || 5000;
 const swaggerOptions = {
     swaggerDefinition: {
         openapi: '3.0.0',
@@ -54,7 +54,7 @@ const swaggerOptions = {
         servers:
             [
                 {
-                    url: process.env.HOST+":"+PORT+'/api/v1'
+                    url: process.env.HOST+':'+PORT+'/api/v1'
                 }
             ],
     },
@@ -63,15 +63,15 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-app.use('/api/v1/hospitals', hospitals);
-app.use('/api/v1/appointments', appointments);
+app.use('/api/v1/cars', cars);
+app.use('/api/v1/bookings', bookings);
 app.use('/api/v1/auth', auth);
 //Cookie parser
 app.use(cookieParser());
 
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'on' + process.env.HOST+" :", PORT));
+
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'on ' + process.env.HOST+' :', PORT));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
